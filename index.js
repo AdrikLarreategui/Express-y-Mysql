3d6500700d31a6857f8951aaf5f8d60835757c00
AdrikLarreategui <adrian15186@hotmail.com>
2023-11-29 19:47:22 +0100

    commit base de datos creada

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const mysql = require('mysql2')
 const PORT = 3000
 app.use(express.json())
 
+const newDataBase = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'M00nch1ldr3n?'
+})
+newDataBase.connect()
+
+app.get('/createdb', (req,res) => {
+    const sql = 'CREATE DATABASE newDataBase'
+    newDataBase.query(sql,(err,result) => {
+        if(err)throw err;
+        console.log(result)
+        res.send('Database created')
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`Server working at port ${PORT} `);
 })
